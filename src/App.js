e2908ec03b7cad8865c72e8613e84354b7c8f123
mdg333 <55594281+mdg333@users.noreply.github.com>
2021-01-15 16:50:53 -0800

    reformat

--- src/App.js
+++ src/App.js
@@ -1,66 +1,66 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 import Table from './Table'
 import Form from './Form';
 import axios from 'axios';
 
 
 class App extends Component {
-  state = {
-    characters: [],
-  }
+    state = {
+        characters: [],
+    }
 
-  removeCharacter = index => {
-  const { characters } = this.state
+    removeCharacter = index => {
+        const {characters} = this.state
 
-  this.setState({
-    characters: characters.filter((character, i) => {
-      return i !== index
-    	}),
-  	})
-  }
+        this.setState({
+            characters: characters.filter((character, i) => {
+                return i !== index
+            }),
+        })
+    }
 
-  handleSubmit = character => {
-   this.makePostCall(character).then( callResult => {
-      if (callResult === true) {
-         this.setState({ characters: [...this.state.characters, character] });
-      }
-   });
-  }
+    handleSubmit = character => {
+        this.makePostCall(character).then(callResult => {
+            if (callResult === true) {
+                this.setState({characters: [...this.state.characters, character]});
+            }
+        });
+    }
 
- 	componentDidMount() {
-   axios.get('http://localhost:5000/users')
-    .then(res => {
-      const characters = res.data.users_list;
-      this.setState({ characters });
-    })
-    .catch(function (error) {
-      //Not handling the error. Just logging into the console.
-      console.log(error);
-    });
-	}
+    componentDidMount() {
+        axios.get('http://localhost:5000/users')
+            .then(res => {
+                const characters = res.data.users_list;
+                this.setState({characters});
+            })
+            .catch(function (error) {
+                //Not handling the error. Just logging into the console.
+                console.log(error);
+            });
+    }
 
-	makePostCall(character){
-   return axios.post('http://localhost:5000/users', character)
-    .then(function (response) {
-      console.log(response);
-      return (response.status === 200);
-    })
-    .catch(function (error) {
-      console.log(error);
-      return false;
-    });
-  }
+    makePostCall(character) {
+        return axios.post('http://localhost:5000/users', character)
+            .then(function (response) {
+                console.log(response);
+                return (response.status === 200);
+            })
+            .catch(function (error) {
+                console.log(error);
+                return false;
+            });
+    }
 
-  render() {
-  const { characters } = this.state
+    render() {
+        const {characters} = this.state
 
-  return (
-	  <div className="container">
-	    <Table characterData={characters} removeCharacter={this.removeCharacter} />
-	    <Form handleSubmit={this.handleSubmit} />
-	  </div>
-	)
-  }
+        return (
+            <div className="container">
+                <Table characterData={characters} removeCharacter={this.removeCharacter}/>
+                <Form handleSubmit={this.handleSubmit}/>
+            </div>
+        )
+    }
 }
 
 export default App
